436b235733d145837068d1c09491b89f63df7dfd
YagmurCelikkaya <ry.celikkaya@gmail.com>
2025-11-14 21:24:39 +0300

    Update ex-2.py

--- ex-2.py
+++ ex-2.py
@@ -1,1 +1,16 @@
+def all_primes(num):
+    primes = []
+    if num < 0:
+        return "There are no negative prime numbers"
+    if num > 1:
+        for prm in range(2, num + 1):
+            for i in range(2 , prm):
+                if prm % i == 0:
+                    break
+            else: 
+                primes.append(prm)
+        return primes
+
+text = int(input("Please enter a number : "))
+print(all_primes(text))
 
