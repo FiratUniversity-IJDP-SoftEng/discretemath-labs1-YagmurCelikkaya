85d6ee3f0c771f611c2f2c2774b47e736b60f217
YagmurCelikkaya <ry.celikkaya@gmail.com>
2025-11-14 21:24:11 +0300

    Update ex-1.py

--- ex-1.py
+++ ex-1.py
@@ -1,1 +1,16 @@
+def prime(num):
+    if num > 1:
+        count = 0
+        for i in range(1, num + 1):
+            if num % i == 0:
+                count += 1
+        if count == 2:
+            print(f"{num} is an prime number")
+        else:
+            print(f"{num} is not an prime number")
+    else:
+        print(f"{num} is not an prime number")
+
+text = int(input("Please enter an number : "))
+prime(text)
 
